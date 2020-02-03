813544d8f4ac1e8c4de0138b6645a5c04dfd287c
rhesadaiva <rhesa.daiva@gmail.com>
2020-02-03 08:04:20 +0700

    membuat view maintenance mode

---
+++ application/views/maintenance.php
@@ -0,0 +1,47 @@
+<!doctype html>
+<title>Site Maintenance</title>
+<style>
+    body {
+        text-align: center;
+        padding: 150px;
+    }
+
+    h1 {
+        font-size: 50px;
+    }
+
+    body {
+        font: 20px Helvetica, sans-serif;
+        color: #333;
+    }
+
+    article {
+        display: block;
+        text-align: left;
+        width: 650px;
+        margin: 0 auto;
+    }
+
+    a {
+        color: #dc8100;
+        text-decoration: none;
+    }
+
+    a:hover {
+        color: #333;
+        text-decoration: none;
+    }
+
+    p {
+        text-align: justify
+    }
+</style>
+
+<article>
+    <h1>Aplikasi sedang dalam proses pemeliharaan!</h1>
+    <div>
+        <p>Dikarenakan adanya penambahan fitur baru dan pemeliharaan aplikasi, maka untuk sementara ini, aplikasi ini akan tidak dapat diakses untuk sementara waktu. Kami akan memberitahukan dengan segera apabila akses aplikasi sudah dibuka kembali. Mohon maaf atas ketidaknyamanannya.</p>
+        <p>Atas perhatiannya, diucapkan terima kasih.</p>
+        <h6>&copy; 2019-<?= date("Y"); ?>. Developed with ♥ by Daiva</h6>
+    </div>
+</article>
