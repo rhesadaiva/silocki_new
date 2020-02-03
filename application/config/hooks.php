c1f440a0c0cbbd96f6f5cefbb72322a26f28e6ad
rhesadaiva <rhesa.daiva@gmail.com>
2020-02-03 08:04:07 +0700

    membuat hook maintenance mode

--- application/config/hooks.php
+++ application/config/hooks.php
@@ -1,5 +1,5 @@
 <?php
-defined('BASEPATH') OR exit('No direct script access allowed');
+defined('BASEPATH') or exit('No direct script access allowed');
 
 /*
 | -------------------------------------------------------------------------
@@ -11,3 +11,11 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 |	https://codeigniter.com/user_guide/general/hooks.html
 |
 */
+
+// Hooks Maintenance Mode
+$hook['pre_system'][] = array(
+    'class'    => 'maintenance_hook',
+    'function' => 'offline_check',
+    'filename' => 'maintenance_hook.php',
+    'filepath' => 'hooks'
+);
