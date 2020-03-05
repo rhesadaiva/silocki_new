176f4ebde11efd0a0a440476c684ff0ab00a0669
rhesadaiva <rhesa.daiva@gmail.com>
2020-03-05 07:29:17 +0700

    menambah halaman log aktivitas

---
+++ application/views/admin/v_logActivity.php
@@ -0,0 +1,38 @@
+<div class="main">
+    <style>
+        tbody>tr {
+            text-align: center;
+        }
+    </style>
+    <!-- MAIN CONTENT -->
+    <div class="main-content">
+        <div class="container-fluid">
+            <!-- Tabel -->
+            <div class="main-content approved">
+                <div class="panel panel-headline panel-primary" id="approved-content">
+                    <div class="panel-heading panel-title">
+                        <div class="col-sm">
+                            <h3 style="margin-top: -10px; margin-bottom: -10px;"><?= $title; ?></h3>
+                        </div>
+                    </div>
+                    <div class="panel-body">
+                        <table class="table table-striped table-hover table-bordered" id="logActivityTable">
+                            <thead>
+                                <tr class="success">
+                                    <th scope="col" class="text-center">Nama Pegawai</th>
+                                    <th scope="col" class="text-center">Deskripsi Log</th>
+                                    <th scope="col" class="text-center">Waktu</th>
+                                </tr>
+                            </thead>
+                            <tbody>
+
+                            </tbody>
+                        </table>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+    <!-- END MAIN CONTENT -->
+
+</div>
